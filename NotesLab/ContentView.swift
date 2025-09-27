707a35f90a69c8d9c9025e4acb84435672b49be0
Bekzat Kalybayev <bekzat92@gmail.com>
2025-09-27 21:30:33 +0500

    Initial Commit

---
+++ NotesLab/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NotesLab
+//
+//  Created by Bekzat Kalybayev on 27.09.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NotesLab/NotesLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesLabApp.swift
+//  NotesLab
+//
+//  Created by Bekzat Kalybayev on 27.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct NotesLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
